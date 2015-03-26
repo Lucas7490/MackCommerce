83eef6a44fd26353add63c03c5c3bbda60130e86
Lucas <Lucas@192.168.51.221>
2015-03-25 22:05:00 -0300

    Segundo Commit

---
+++ MackCommerce-war/src/java/br/mack/controller/command/FactoryMethod.java
@@ -0,0 +1,9 @@
+package br.mack.controller.command;
+/**
+ *
+ * @author Grupo 01 - Tema: Loja virtual
+ */
+public class FactoryMethod {
+    public Command getCommand() {return null;}
+    public Command findCommand() {return null;}
+}
---
+++ MackCommerce/src/br/mack/dao/PedidoDAO.java
@@ -0,0 +1,31 @@
+package br.mack.dao;
+
+import br.mack.modeloEntidade.Pedido;
+import java.util.Set;
+/**
+ *
+ * @author Grupo 01 - Tema: Loja virtual
+ */
+public class PedidoDAO extends DAO<Pedido>{
+
+    @Override
+    public void create(Pedido el) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Pedido read(Pedido key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Pedido delete(Pedido key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Pedido key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public Set<Pedido> readAll() {return null;}
+}
